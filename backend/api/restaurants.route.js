1de589f260c1daaf9e3d691c99b0c1e3fcf5004e
Gabriel Chen <gabriel.jchen@gmail.com>
2021-06-15 15:23:02 -0400

    add review routes

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -5,4 +5,10 @@ const router = express.Router();
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
 
+router
+  .route("/review")
+  .post(ReviewsCtrl / apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview);
+
 export default router;
--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -13,7 +13,7 @@ export default class RestaurantsDAO {
         .collection("restaurants");
     } catch (e) {
       console.error(
-        `Unable to establish a collection handle in restaurantDAO: ${e}`
+        `Unable to establish a collection handle in restaurantsDAO: ${e}`
       );
     }
   }
@@ -23,38 +23,40 @@ export default class RestaurantsDAO {
     page = 0,
     restaurantsPerPage = 20,
   } = {}) {
-    let query
+    let query;
     if (filters) {
       if ("name" in filters) {
-        query = { $text: { $search: filters["name"] } }
+        query = { $text: { $search: filters["name"] } };
       } else if ("cuisine" in filters) {
-        query = { "cuisine": { $eq: filters["cuisine"] } }
+        query = { cuisine: { $eq: filters["cuisine"] } };
       } else if ("zipcode" in filters) {
-        query = { "address.zipcode": { $eq: filters["zipcode"] } }
+        query = { "address.zipcode": { $eq: filters["zipcode"] } };
       }
     }
 
-    let cursor
-    
+    let cursor;
+
     try {
-      cursor = await restaurants
-        .find(query)
+      cursor = await restaurants.find(query);
     } catch (e) {
-      console.error(`Unable to issue find command, ${e}`)
-      return { restaurantsList: [], totalNumRestaurants: 0 }
+      console.error(`Unable to issue find command, ${e}`);
+      return { restaurantsList: [], totalNumRestaurants: 0 };
     }
 
-    const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
+    const displayCursor = cursor
+      .limit(restaurantsPerPage)
+      .skip(restaurantsPerPage * page);
 
     try {
-      const restaurantsList = await displayCursor.toArray()
-      const totalNumRestaurants = await restaurants.countDocuments(query)
+      const restaurantsList = await displayCursor.toArray();
+      const totalNumRestaurants = await restaurants.countDocuments(query);
 
-      return { restaurantsList, totalNumRestaurants }
+      return { restaurantsList, totalNumRestaurants };
     } catch (e) {
       console.error(
-        `Unable to convert cursor to array or problem counting documents, ${e}`,
-      )
-      return { restaurantsList: [], totalNumRestaurants: 0 }
+        `Unable to convert cursor to array or problem counting documents, ${e}`
+      );
+      return { restaurantsList: [], totalNumRestaurants: 0 };
     }
   }
+}
